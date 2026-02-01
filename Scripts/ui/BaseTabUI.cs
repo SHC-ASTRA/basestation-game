4ecce8da8128a635a612a0abcbda912aceb25e0a
Roald Schaum <178021507+Roald-Schaum@users.noreply.github.com>
2026-02-01 13:43:40 -0600

    Small bugfixes, info pane work

--- Scripts/ui/BaseTabUI.cs
+++ Scripts/ui/BaseTabUI.cs
@@ -5,127 +5,128 @@ using System.Threading.Tasks;
 
 namespace ui
 {
-	// Base tab UI. Abstractly handles ROS functions and provides a controller input interface.
-	public abstract partial class BaseTabUI : Control
-	{
-		public CancellationTokenSource CTS = new();
-		// ROS stuff
-		[Export]
-		public bool ROSDependent;
+    // Base tab UI. Abstractly handles ROS functions and provides a controller input interface.
+    public abstract partial class BaseTabUI : Control
+    {
+        public CancellationTokenSource CTS = new();
+        // ROS stuff
+        [Export]
+        public bool ROSDependent;
 
-		protected int Rate = 50;
-		public int Delay;
-		private Task Updater;
+        protected int Rate = 50;
+        public int Delay;
+        private Task Updater;
 
-		// Controller
-		protected static bool UpButtonDown, DownButtonDown, LeftButtonDown, RightButtonDown;
-		protected static bool UpButton, DownButton, LeftButton, RightButton;
+        // Controller
+        protected static bool UpButtonDown, DownButtonDown, LeftButtonDown, RightButtonDown;
+        protected static bool UpButton, DownButton, LeftButton, RightButton;
 
-		protected static bool StartButton, BackButton;
+        protected static bool StartButton, BackButton;
 
-		protected static bool LeftStickPress, RightStickPress;
+        protected static bool LeftStickPress, RightStickPress;
 
-		protected static float AButton, BButton, XButton, YButton;
+        protected static float AButton, BButton, XButton, YButton;
 
-		protected static float BlackButton, WhiteButton;
+        protected static float BlackButton, WhiteButton;
 
-		protected static float LeftTrigger, RightTrigger;
+        protected static float LeftTrigger, RightTrigger;
 
-		protected static float LeftBumper, RightBumper;
+        protected static float LeftBumper, RightBumper;
 
-		protected static Vector2 LeftStick, RightStick;
+        protected static Vector2 LeftStick, RightStick;
 
-		public override void _Ready()
-		{
-			// Start or stop the ROS emitter if this node is visible
-			this.VisibilityChanged += () => { if (this.Visible) Resume(); else Pause(); };
+        public override void _Ready()
+        {
+            // Start or stop the ROS emitter if this node is visible
+            this.VisibilityChanged += () => { if (this.Visible) Resume(); else Pause(); };
 
-			// Spool this tab in the ROS ecosystem
-			Task.Run(async () =>
-			{
-				await ROS.AwaitRosReady();
-				AdvertiseToROS();
-				Updater = new Task(() => Update(CTS.Token), CTS.Token);
-				if (CallDeferred("is_visible").AsBool())
-					Updater.Start();
-			});
+            // Spool this tab in the ROS ecosystem
+            Task.Run(async () =>
+            {
+                await ROS.AwaitRosReady();
+                AdvertiseToROS();
+                Updater = new Task(() => Update(CTS.Token), CTS.Token);
+                if (CallDeferred("is_visible").AsBool())
+                    Updater.Start();
+            });
 
-			base._Ready();
-		}
+            base._Ready();
+        }
 
-		public override void _Process(double delta)
-		{
-			base._Process(delta);
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
 
-			UpButtonDown = Input.IsActionJustPressed("UpBtn");
-			DownButtonDown = Input.IsActionJustPressed("DownBtn");
-			LeftButtonDown = Input.IsActionJustPressed("LeftBtn");
-			RightButtonDown = Input.IsActionJustPressed("RightBtn");
+            UpButtonDown = Input.IsActionJustPressed("UpBtn");
+            DownButtonDown = Input.IsActionJustPressed("DownBtn");
+            LeftButtonDown = Input.IsActionJustPressed("LeftBtn");
+            RightButtonDown = Input.IsActionJustPressed("RightBtn");
 
-			UpButton = Input.IsActionPressed("UpBtn");
-			DownButton = Input.IsActionPressed("DownBtn");
-			LeftButton = Input.IsActionPressed("LeftBtn");
-			RightButton = Input.IsActionPressed("RightBtn");
+            UpButton = Input.IsActionPressed("UpBtn");
+            DownButton = Input.IsActionPressed("DownBtn");
+            LeftButton = Input.IsActionPressed("LeftBtn");
+            RightButton = Input.IsActionPressed("RightBtn");
 
-			StartButton = Input.IsActionJustPressed("StartBtn");
-			BackButton = Input.IsActionJustPressed("BackBtn");
+            StartButton = Input.IsActionJustPressed("StartBtn");
+            BackButton = Input.IsActionJustPressed("BackBtn");
 
-			LeftStickPress = Input.IsActionPressed("LStickPress");
-			RightStickPress = Input.IsActionPressed("RStickPress");
+            LeftStickPress = Input.IsActionPressed("LStickPress");
+            RightStickPress = Input.IsActionPressed("RStickPress");
 
-			AButton = Input.GetActionStrength("ABtn");
-			BButton = Input.GetActionStrength("BBtn");
-			XButton = Input.GetActionStrength("XBtn");
-			YButton = Input.GetActionStrength("YBtn");
+            AButton = Input.GetActionStrength("ABtn");
+            BButton = Input.GetActionStrength("BBtn");
+            XButton = Input.GetActionStrength("XBtn");
+            YButton = Input.GetActionStrength("YBtn");
 
-			BlackButton = Input.GetActionStrength("BlackBtn");
-			WhiteButton = Input.GetActionStrength("WhiteBtn");
+            BlackButton = Input.GetActionStrength("BlackBtn");
+            WhiteButton = Input.GetActionStrength("WhiteBtn");
 
-			LeftTrigger = Input.GetActionStrength("LTrigger");
-			RightTrigger = Input.GetActionStrength("RTrigger");
+            LeftTrigger = Input.GetActionStrength("LTrigger");
+            RightTrigger = Input.GetActionStrength("RTrigger");
 
-			LeftBumper = Input.GetActionStrength("LBumper");
-			RightBumper = Input.GetActionStrength("RBumper");
+            LeftBumper = Input.GetActionStrength("LBumper");
+            RightBumper = Input.GetActionStrength("RBumper");
 
-			LeftStick = Input.GetVector("LStickHN", "LStickHP", "LStickVN", "LStickVP");
-			RightStick = Input.GetVector("RStickHN", "RStickHP", "RStickVN", "RStickVP");
-		}
+            LeftStick = Input.GetVector("LStickHN", "LStickHP", "LStickVN", "LStickVP");
+            RightStick = Input.GetVector("RStickHN", "RStickHP", "RStickVN", "RStickVP");
+        }
 
-		// Runs ROS emitter async to the main thread, slightly
-		// more performant doesn't run until ROS is ready
-		private async void Update(CancellationToken token)
-		{
-			while (true)
-			{
-				if (token.IsCancellationRequested)
-					return;
-				await Task.Delay(Rate + Delay);
-				Delay = 0;
-				EmitToROS();
-			}
-		}
+        // Runs ROS emitter async to the main thread, slightly
+        // more performant doesn't run until ROS is ready
+        private async void Update(CancellationToken token)
+        {
+            while (true)
+            {
+                if (token.IsCancellationRequested)
+                    return;
+                await Task.Delay(Rate + Delay);
+                Delay = 0;
+                EmitToROS();
+            }
+        }
 
-		public async void Pause()
-		{
-			await CTS.CancelAsync();
-			Updater.Dispose();
-		}
+        public async void Pause()
+        {
+            await CTS.CancelAsync();
+            if (Updater != null)
+                Updater.Dispose();
+        }
 
-		public void Resume()
-		{
-			CTS.Dispose();
-			CTS = new CancellationTokenSource();
-			Updater = new Task(() => Update(CTS.Token), CTS.Token);
-			Updater.Start();
-		}
+        public void Resume()
+        {
+            CTS.Dispose();
+            CTS = new CancellationTokenSource();
+            Updater = new Task(() => Update(CTS.Token), CTS.Token);
+            Updater.Start();
+        }
 
-		/// <summary> Called on Ready, used to advertise associated interfaces to ROS </summary>
-		public abstract void AdvertiseToROS();
+        /// <summary> Called on Ready, used to advertise associated interfaces to ROS </summary>
+        public abstract void AdvertiseToROS();
 
-		/// <summary> Called every (<paramref name="Slow"/>)ms, sends control data to ROS. Can be changed on a per-tab basis </summary>
-		public abstract void EmitToROS();
+        /// <summary> Called every (<paramref name="Slow"/>)ms, sends control data to ROS. Can be changed on a per-tab basis </summary>
+        public abstract void EmitToROS();
 
-		/// <summary> Cleanup our controls so that clucky doesn't do something stupid while we're offline </summary>
-		public override abstract void _ExitTree();
-	}
+        /// <summary> Cleanup our controls so that clucky doesn't do something stupid while we're offline </summary>
+        public override abstract void _ExitTree();
+    }
 }
--- Scripts/ui/prefabs/InfoPane.cs
+++ Scripts/ui/prefabs/InfoPane.cs
@@ -1,11 +1,40 @@
 using Godot;
+using System.Linq;
 
-public partial class InfoPane : Panel
+public partial class InfoPane : OptionButton
 {
+    private long lastSelected;
+    private long[] lastChildSelections;
+    OptionButton[] ChildButtons;
+    private Node lastInfoParent;
+    public HBoxContainer InfoContainer;
+
     public override void _Ready()
     {
-        ItemRectChanged += () => SetSize(new Vector2((GetParent() as Control).Size.Y, (GetParent() as Control).Size.Y));
+        ChildButtons = [.. GetChildren().Where(static _ => _ is OptionButton).Cast<OptionButton>()];
+        lastChildSelections = new long[ChildButtons.Length];
 
+        ItemSelected += (_) =>
+        {
+            ChildButtons[lastSelected].Visible = false;
+            ChildButtons[Selected].Visible = true;
+            lastSelected = Selected;
+        };
 
+        for (int i = 0; i < ChildButtons.Length; i++)
+        {
+            ChildButtons[i].ItemSelected += (_) =>
+            {
+                (ChildButtons[i].GetChildren()[(int)lastChildSelections[i]] as Control).Visible = false;
+                InfoContainer.GetChild(-1).Reparent(lastInfoParent);
+
+                Control c = ChildButtons[i].GetChildren()[ChildButtons[i].Selected] as Control;
+                lastInfoParent = c.GetParent();
+                c.Reparent(InfoContainer);
+                c.Visible = true;
+
+                lastChildSelections[i] = ChildButtons[i].Selected;
+            };
+        }
     }
 }
