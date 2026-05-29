7c23a839b8152979b562e385351650730cf95b3e
Roald Schaum <178021507+Roald-Schaum@users.noreply.github.com>
2026-05-29 01:17:27 -0600

    Cameras work now

--- Scripts/ui/tabs/CamsTabUI.cs
+++ Scripts/ui/tabs/CamsTabUI.cs
@@ -1,127 +1,132 @@
 using Godot;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.IO;
 
 namespace UI
 {
-	public partial class CamsTabUI : BaseTabUI
-	{
-		[Export]
-		VBoxContainer Streams, Addstream, Grid;
+    public partial class CamsTabUI : BaseTabUI
+    {
+        [Export]
+        VBoxContainer Streams, Grid;
 
-		[Export]
-		MenuButton Mode;
-		int mode = 0;
+        [Export]
+        MenuButton Mode;
+        int mode = 0;
 
-		[Export]
-		public LineEdit ID, Address;
+        [Export]
+        public LineEdit ID;
 
-		[Export]
-		public SpinBox Rows, Columns;
+        [Export]
+        public SpinBox Rows, Columns;
 
-		[Export]
-		public Button Commit;
-		[Export]
-		public TextureRect Committing;
+        [Export]
+        public Button Commit;
+        [Export]
+        public TextureRect Committing;
 
-		[Export]
-		public TextEdit ActiveCams;
-		[Export]
-		public Button Query;
+        [Export]
+        public TextEdit ActiveCams;
+        [Export]
+        public Button Query;
 
-		private static Socket Sock;
+        private static Socket Sock;
 
-		private HashSet<string> CamTable = [];
+        private HashSet<string> CamTable = [];
 
-		public override void _Ready()
-		{
-			try
-			{
-				Sock = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
-				Sock.Connect(new UnixDomainSocketEndPoint("/tmp/basestation-cameras-ipc"));
-			}
-			catch { GD.PushError("Failed to open the IPC stream"); }
+        public override void _Ready()
+        {
+            PopupMenu p = Mode.GetPopup();
+            p.IndexPressed += (id) =>
+            {
+                switch (id)
+                {
+                    case 0:
+                        Streams.Visible = true;
+                        Grid.Visible = false;
+                        break;
+                    case 1:
+                        Streams.Visible = false;
+                        Grid.Visible = true;
+                        break;
+                }
+                Mode.Text = p.GetItemText((int)id);
+                mode = (int)id;
+            };
+            Commit.Pressed += () =>
+            {
+                string payload = "";
+                string idText = ID.Text;
+                switch (mode)
+                {
+                    case 0: // Add Stream
+                        payload = $"launch {idText}";
+                        if (CamTable.Contains(idText))
+                        {
+                            GD.Print("Cannot add stream, ID already in camera table!");
+                            return;
+                        }
+                        break;
+                    case 1: // Grid
+                        payload = $"grid {Rows.Value} {Columns.Value}";
+                        break;
+                }
+                OS.CreateProcess("bash", ["-c", $"cameracli {payload}"]);
+                // Sock.Send(Encoding.UTF8.GetBytes(payload));
+                Committing.Visible = true;
+            }
+                        ;
+            Commit.ButtonUp += () => Committing.Visible = false;
+            Query.Pressed += async () =>
+            {
+                lock (CamTable)
+                {
+                    CamTable = ["Active Streams:\n"];
+                    CamTable = QueryCams().Result.AsEnumerable() as HashSet<string>;
+                }
+                ActiveCams.Text = CamTable.Aggregate((a, b) =>
+                {
+                    return $"{a}\n{b}";
+                });
+            };
+        }
 
-			PopupMenu p = Mode.GetPopup();
-			p.IndexPressed += (id) =>
-			{
-				switch (id)
-				{
-					case 0:
-						Streams.Visible = true;
-						Addstream.Visible = true;
-						Grid.Visible = false;
-						break;
-					case 1:
-						Streams.Visible = true;
-						Addstream.Visible = false;
-						Grid.Visible = false;
-						break;
-					case 2:
-						Streams.Visible = false;
-						Grid.Visible = true;
-						break;
-				}
-				Mode.Text = p.GetItemText((int)id);
-				mode = (int)id;
-			};
-			Commit.Pressed += () =>
-			{
-				string payload = "";
-				string idText = ID.Text;
-				switch (mode)
-				{
-					case 0: // Add Stream
-						payload = $"{{\"cmd\":\"add_stream\",\"payload\":{{\"id\":\"{idText}\",\"url\":\"rtsp://{Address.Text}\"}}";
-						if (CamTable.Contains(idText))
-						{
-							GD.Print("Cannot add stream, ID already in camera table!");
-							return;
-						}
-						break;
-					case 1: // Remove Stream
-						payload = $"{{\"cmd\":\"remove_stream\",\"payload\":{{\"id\":\"{idText}\"}}";
-						if (!CamTable.Contains(idText))
-						{
-							GD.Print("Cannot remove stream, not in Camera table!");
-							return;
-						}
-						break;
-					case 2: // Grid
-						payload = $"{{\"cmd\":\"grid\",\"payload\":{{\"rows\":{Rows.Value},\"cols\":{Columns.Value}}}";
-						break;
-				}
-				Sock.Send(Encoding.UTF8.GetBytes(payload));
-				Committing.Visible = true;
-			};
-			Commit.ButtonUp += () => Committing.Visible = false;
-			Query.Pressed += async () =>
-			{
-				lock (CamTable)
-				{
-					CamTable = ["Active Streams:\n"];
-					CamTable = QueryCams().Result.AsEnumerable() as HashSet<string>;
-				}
-				ActiveCams.Text = CamTable.Aggregate((a, b) =>
-				{
-					return $"{a}\n{b}";
-				});
-			};
-		}
+        public static async Task<string[]> QueryCams()
+        {
+            OS.CreateProcess("bash", ["-c", "cameracli list > /tmp/cameralist"]);
+            await Task.Delay(50);
+            // Silly little aggregator statement takes the first two characters off each string in the read-in array
+            return File.ReadAllLines("/tmp/cameralist")[..][2..];
+        }
 
-		public static async Task<string[]> QueryCams()
-		{
-			OS.CreateProcess("bash", ["-c", "cameracli list > /tmp/cameralist"]);
-			// Silly little aggregator statement takes the first two characters off each string in the read-in array
-			return File.ReadAllLines("/tmp/cameralist")[..][2..];
-		}
+        public override void EmitToROS() { }
 
-		public override bool AdvertiseToROS() { return false; }
-		public override void EmitToROS() { }
-		public override void _ExitTree() { }
-	}
+        private static bool tried = false;
+        public override void STARTTAB()
+        {
+        retry:
+            try
+            {
+                Sock = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+                Sock.Connect(new UnixDomainSocketEndPoint("/tmp/basestation-cameras-ipc"));
+            }
+            catch
+            {
+                if (!tried)
+                {
+                    Sock.Dispose();
+                    GD.PushWarning("Failed to open the IPC stream, attempting to open basestation-cameras?");
+                    OS.CreateProcess("bash", ["-c", "basestation-cameras"]);
+                    Thread.Sleep(250);
+                    tried = true;
+                    goto retry;
+                }
+                else GD.PushError("Failed to open the Basestation cameras and/or the IPC stream stream.");
+            }
+        }
+        public override void STOPTAB() { }
+    }
 }
